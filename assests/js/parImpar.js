7fd2ac58a51e37a7c523be2c5fc603bac141d1d7
Mateo Cuella <cuellamateo@gmai.com>
2024-05-03 19:36:09 -0300

    se mejoro el control para parImpar.js

--- assests/js/parImpar.js
+++ assests/js/parImpar.js
@@ -1,9 +1,15 @@
-let numero = prompt("Ingrese un número").replace(",", ".");
+// Pido al usuario un numero y lo convierto a decimal, en caso de tener comas, las reemplaza por puntos
+let input = prompt("Ingrese un número").replace(",", ".");
+let numero = parseFloat(input);
 
-while (isNaN(numero) || numero.trim() === "") {
+//Control que pide nuevamente al usuario que ingrese un número si el input no es un número o contiene espacios y/o letras
+while (isNaN(numero) || input !== numero.toString()) {
   console.log("Ingrese un número válido");
-  numero = prompt("Ingrese un número").replace(",", ".");
+  input = prompt("Ingrese un número").replace(",", ".");
+  numero = parseFloat(input);
 }
+//Muestra el número ingresado y devuelve un mensaje diciendo si es par o impar dependiendo de si el resto es 0 o no
+console.log(numero);
 if (numero % 2 == 0) {
   console.log("El número es par");
 } else {
