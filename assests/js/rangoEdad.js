47a2c44dae86c799b6ae33f6ade2b3d879207c0c
Mateo Cuella <cuellamateo@gmai.com>
2024-05-03 18:44:55 -0300

    se mejoro el control del prompt en rangoEdad.js

--- assests/js/rangoEdad.js
+++ assests/js/rangoEdad.js
@@ -1,9 +1,27 @@
-let edad = parseFloat(prompt("Ingrese su edad").replace(",", "."));
+// Pido al usuario un numero y lo convierto a decimal, en caso de tener comas, las reemplaza por puntos
+let input = prompt("Ingrese su edad").replace(",", ".");
+let edad = parseFloat(input);
 
-while (edad < 0 || Number.isInteger(edad) == false || isNaN(edad)) {
+/*
+Este control hace que vuelva a pedir la edad en caso de que:
+- Al convertir el input en número estos dos valores sean diferentes, entonces si el usuario pone "15a", parseFloat lo convierte a "15", pero al ser diferente que el input original, da error. Esto también sirve si el usuario pone espacios
+- La edad es negativa
+- La edad no es un numero entero
+- La edad no es un número
+*/
+
+while (
+  input !== edad.toString() ||
+  edad < 0 ||
+  !Number.isInteger(edad) ||
+  isNaN(edad)
+) {
   console.log("Ingrese una edad válida");
-  edad = parseFloat(prompt("Ingrese su edad").replace(",", "."));
+  input = prompt("Ingrese su edad").replace(",", ".");
+  edad = parseFloat(input);
 }
+
+// Muestra por consola los resultados de las operaciones
 console.log(edad);
 if (edad >= 0 && edad <= 12) {
   console.log("Usted es un niño");
